edefc4d4b2bb91773ff798830ab88b3cf415ba21
bvlaming <29951819+bvlaming@users.noreply.github.com>
2022-12-01 09:30:21 +0100

    Day 1

---
+++ aoc/src/main/scala/Main.scala
@@ -0,0 +1,7 @@
+package advent
+
+@main def advent_start: Unit =
+  println("Doing stuff")
+  day1
+
+def msg = "I was compiled by Scala 3. :)"
---
+++ aoc/src/main/scala/days/day1.scala
@@ -0,0 +1,33 @@
+package advent
+
+import scala.io.Source
+
+val separator: String = " "
+val data1a: List[String] = Source.fromResource("day1.csv").getLines.toList
+
+def caloriesPerElf(inputData: List[String]): List[Int] = {
+  def splitLists(data:List[String], collector: List[List[String]]): List[List[String]] = {
+    val (preList, postList) = data.span(_ != "")
+    val collNew = preList :: collector
+    if (preList.isEmpty) collNew
+    else splitLists(postList.drop(1), collNew)
+  }
+   splitLists(inputData, List(List.empty)).drop(1).map(sumCals)
+  }
+
+def sumCals(input: List[String]): Int = {
+  input.map(_.toInt).sum
+  }
+
+def day1a: Int = {
+    caloriesPerElf(data1a).max
+  }
+
+def day1b: Int = {
+    caloriesPerElf(data1a).sortBy(c => -c).take(3).sum
+  }
+
+def day1: Unit = {
+    println(day1a)
+    println(day1b)
+  }
