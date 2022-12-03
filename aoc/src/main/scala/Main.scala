b1904e21a5d458dbc7d478f767c5c298e8a46efd
bvlaming <29951819+bvlaming@users.noreply.github.com>
2022-12-03 09:39:24 +0100

    Linting

--- aoc/src/main/scala/Main.scala
+++ aoc/src/main/scala/Main.scala
@@ -1,7 +1,4 @@
 package advent
 
 @main def advent_start: Unit =
-  println("Doing stuff")
   day3
-
-def msg = "I was compiled by Scala 3. :)"
--- aoc/src/main/scala/days/day1.scala
+++ aoc/src/main/scala/days/day1.scala
@@ -8,28 +8,31 @@ val data1a: List[String] = Source.fromResource("day1.csv").getLines.toList
 def caloriesPerElf(inputData: List[String]): List[Int] = {
 
   @tailrec
-  def splitLists(data:List[String], collector: List[List[String]]): List[List[String]] = {
+  def splitLists(
+      data: List[String],
+      collector: List[List[String]]
+  ): List[List[String]] = {
     val (preList, postList) = data.span(_ != "")
     val collNew = preList :: collector
     if (postList.isEmpty) collNew
     else splitLists(postList.drop(1), collNew)
   }
-   splitLists(inputData, List(List.empty)).map(sumCals)
-  }
+  splitLists(inputData, List(List.empty)).map(sumCals)
+}
 
 def sumCals(input: List[String]): Int = {
   input.map(_.toInt).sum
-  }
+}
 
 def day1a: Int = {
-    caloriesPerElf(data1a).max
-  }
+  caloriesPerElf(data1a).max
+}
 
 def day1b: Int = {
-    caloriesPerElf(data1a).sortBy(c => -c).take(3).sum
-  }
+  caloriesPerElf(data1a).sortBy(c => -c).take(3).sum
+}
 
 def day1: Unit = {
-    println(day1a)
-    println(day1b)
-  }
+  println(day1a)
+  println(day1b)
+}
